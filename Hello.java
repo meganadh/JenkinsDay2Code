bfd30eca2539fcc32e256f7b196bddc33d4a4351
Meganadh <E004787@cigniti.com>
2021-09-09 16:35:35 +0530

    day-3 checkin3

--- Hello.java
+++ Hello.java
@@ -4,6 +4,6 @@ class Hello
   {
     System.out.println("Hello");
     System.out.println("Hope upon check-in job will be triggered");
-    System.out.println("Yes the job is triggered");
+    System.out.println("Yes the job is triggered")
   }
 }
